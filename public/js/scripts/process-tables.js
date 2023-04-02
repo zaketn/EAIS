739d2ce0f1cb037f1dec0c75159c779741e7480a
zaketn <specot70@gmail.com>
2023-04-02 12:29:29 +0500

    Fix parse single file to separate xslx document without headers

--- public/js/scripts/process-tables.js
+++ public/js/scripts/process-tables.js
@@ -5,24 +5,12 @@ $.ajaxSetup({
 });
 
 const tables = $('table');
-
 let tableHeader = $(tables[0]).prev().prev().find('span b').html();
-let caption = tables[0].createCaption();
-caption.textContent = $(tables[0]).prev().prev().find('span b').html();
 
 tables.each(function () {
-    let prevPrev = $(this).prev().prev().find('span');
-    let prev = $(this).prev().find('span');
-
-    if (prev.length > 0 && prevPrev > 0) {
-        console.log(prevPrev);
-        let isContinue = prev.html().includes('Продолжение') || prevPrev.html().includes('Продолжение');
-
-        console.log(prevPrev.children('b').length > 0 || prev.children('b').length > 0);
         $.post('process', {
-            'header': '',
+            'header': tableHeader,
             'tables': this.outerHTML
         })
             .done(() => console.log('Данные успешно обработаны.'));
-    }
 })
