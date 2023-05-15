10c6ee12d217ce030892f6d265d39fcfb834a65b
m9kish <Git103153>
2023-05-15 23:20:20 +0500

    Сделал первоначальную верстку

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\IndexController;
 use App\Http\Controllers\ProcessController;
 use App\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Support\Facades\Route;
@@ -8,3 +9,5 @@ Route::get('/process', [ProcessController::class, 'index'])->name('index');
 Route::post('/process', [ProcessController::class, 'process'])
     ->withoutMiddleware(VerifyCsrfToken::class)
     ->name('process');
+
+Route::get('/', [IndexController::class, 'home']);
