9f550d70e1d3a197b5b6a8e78e64355f9c7248fb
zaketn <specot70@gmail.com>
2023-11-26 21:22:55 +0500

    feat: Добавлена генерация тестового пользователя

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -10,11 +10,6 @@ use Illuminate\Support\Str;
  */
 class UserFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     public function definition(): array
     {
         return [
@@ -26,13 +21,18 @@ class UserFactory extends Factory
         ];
     }
 
-    /**
-     * Indicate that the model's email address should be unverified.
-     */
     public function unverified(): static
     {
         return $this->state(fn(array $attributes) => [
             'email_verified_at' => null,
+        ]);
+    }
+
+    public function admin(): static
+    {
+        return $this->state(fn(array $attributes) => [
+            'email' => 'admin@admin.com',
+            'role_id' => 1
         ]);
     }
 }
