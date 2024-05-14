a1cb7e99e60e51e3871a6699a5ac44e6f83cb36f
zaketn <specot70@gmail.com>
2024-05-15 00:49:49 +0500

    refactor: Рефакторинг vue-router

--- routes/api.php
+++ routes/api.php
@@ -7,25 +7,27 @@ use App\Http\Controllers\TableController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth:sanctum'])->get('/user', [UserController::class, 'current'])
-    ->name('api.user');
+Route::middleware(['auth:sanctum'])->group(function() {
+    Route::get('/user', [UserController::class, 'current'])
+        ->name('api.user');
 
-Route::resource('tables', TableController::class)->only([
-    'index', 'show', 'store'
-]);
+    Route::resource('tables', TableController::class)->only([
+        'index', 'show', 'store'
+    ]);
 
-Route::get('/users/current', [UserController::class, 'current']);
-Route::resource('/users', UserController::class);
+    Route::get('/users/current', [UserController::class, 'current']);
+    Route::resource('/users', UserController::class);
 
-Route::resource('/calculator-parameters', CalculatorParametersController::class);
+    Route::resource('/calculator-parameters', CalculatorParametersController::class);
 
-Route::get('/history/{id}', [CalculatorHistoryController::class, 'getRecord']);
-Route::post('/calculator-history/save-history', [CalculatorHistoryController::class, 'saveData']);
-Route::resource('/calculator-history', CalculatorHistoryController::class);
+    Route::get('/history/{id}', [CalculatorHistoryController::class, 'getRecord']);
+    Route::post('/calculator-history/save-history', [CalculatorHistoryController::class, 'saveData']);
+    Route::resource('/calculator-history', CalculatorHistoryController::class);
 
-Route::controller(StatisticsController::class)->prefix('/statistics')->group(function() {
-    Route::post('/by-regions', 'byRegions');
-    Route::post('/by-company-type', 'byCompanyType');
-    Route::post('/by-support-type', 'bySupportType');
-    Route::post('/years', 'getYears');
+    Route::controller(StatisticsController::class)->prefix('/statistics')->group(function() {
+        Route::post('/by-regions', 'byRegions');
+        Route::post('/by-company-type', 'byCompanyType');
+        Route::post('/by-support-type', 'bySupportType');
+        Route::post('/years', 'getYears');
+    });
 });
