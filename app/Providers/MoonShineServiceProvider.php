9039cced6fc9aff732e8e153d7d3289fe7a49c5e
zaketn <specot70@gmail.com>
2024-05-14 23:28:48 +0500

    build: Установлена админ-панель

---
+++ app/Providers/MoonShineServiceProvider.php
@@ -0,0 +1,62 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Providers;
+
+use MoonShine\Providers\MoonShineApplicationServiceProvider;
+use MoonShine\MoonShine;
+use MoonShine\Menu\MenuGroup;
+use MoonShine\Menu\MenuItem;
+use MoonShine\Resources\MoonShineUserResource;
+use MoonShine\Resources\MoonShineUserRoleResource;
+use MoonShine\Contracts\Resources\ResourceContract;
+use MoonShine\Menu\MenuElement;
+use MoonShine\Pages\Page;
+use Closure;
+
+class MoonShineServiceProvider extends MoonShineApplicationServiceProvider
+{
+    /**
+     * @return list<ResourceContract>
+     */
+    protected function resources(): array
+    {
+        return [];
+    }
+
+    /**
+     * @return list<Page>
+     */
+    protected function pages(): array
+    {
+        return [];
+    }
+
+    /**
+     * @return Closure|list<MenuElement>
+     */
+    protected function menu(): array
+    {
+        return [
+            MenuItem::make(
+                static fn() => 'Пользователи',
+                new MoonShineUserResource()
+            ),
+            MenuItem::make(
+                static fn() => __('moonshine::ui.resource.role_title'),
+                new MoonShineUserRoleResource()
+            ),
+
+            MenuItem::make('Документация', 'https://moonshine-laravel.com', blank: true),
+        ];
+    }
+
+    /**
+     * @return Closure|array{css: string, colors: array, darkColors: array}
+     */
+    protected function theme(): array
+    {
+        return [];
+    }
+}
